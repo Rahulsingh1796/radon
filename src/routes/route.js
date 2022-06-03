51535f4c2ecd8d65f8c8faf1bb25775514e603bd
Rahulsingh1796 <rsjadon007@gmail.com>
2022-06-03 20:44:56 +0530

    players name

--- src/routes/route.js
+++ src/routes/route.js
@@ -59,6 +59,35 @@ router.get('/hello', function (req,res) {
     res.send ('this is hello api!')
 
     });
+    
+    let players = []
+
+router.post('/players', function (req, res) {
+    
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
 
 module.exports = router;
 // adding this comment for no reason
